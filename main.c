8290cd4f7d520803d42c1730e11d0592a2f626f9
Amr Elzawawy <amr22.elzawawy@ymail.com>
2018-10-27 01:30:04 +0200

    Main.c cleaned. Ready to work with.

--- main.c
+++ main.c
@@ -4,23 +4,11 @@
  * Created On : 10-10-18
  ***/
 
-
 #include "shell.h"
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "parser.h"
 void main(void)
 {
-//     char *args[20];
-//     printf(">>");
-//     char * command = (char *) malloc(50*sizeof(char));
-//     fgets(command,50,stdin);     
-//     parseLine(command, args);
-//    int res = execvp(*args, args);
-while(1)
-{
-    openShellSession();
-}
-
+    while (1)
+    {
+        openShellSession();
+    }
 }
